c8b818cd86541bf7a3f99f635d8c9963745d5368
richardR144 <richard.rigollet@lapiscine.pro>
2025-04-22 15:52:59 +0200

    création de home, contact, about, delete PageController et .html.twig

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;  //je déclare l'espace de nom de la classe PageController
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;  //j'importe la classe AbstractController de Symfony
+//et abstractController sert à créer des contrôleurs dans Symfony
 use Symfony\Component\HttpFoundation\Response;  //j'importe la classe Response de Symfony
 use Symfony\Component\Routing\Annotation\Route;  //j'importe la classe Route de Symfony
 
@@ -26,6 +27,12 @@ class PageController extends AbstractController  //je crée une classe PageContr
     public function about(): Response //je crée une méthode about qui renvoie une réponse HTTP
     {
         return $this->render('about.html.twig'); //je renvoie le fichier about.html.twig
+    }
+
+    #[Route('/delete', name:"delete")] //je crée une route qui correspond à /delete et qui appelle la méthode delete
+    public function delete(): Response //je crée une méthode delete qui renvoie une réponse HTTP
+    {
+        return $this->render('delete.html.twig'); //je renvoie le fichier delete.html.twig
     }
 }
 
